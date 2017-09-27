fa21d9de5b7e04162b826be90ce749f825c9c362
dcr1586@inbox.lv <dcr1586@inbox.lv>
2017-09-27 14:08:36 +0300

    20170927_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#3. piemers
+NAME="Vards Uzvards"
+echo #NAME
+unset NAME
+echo $NAME
+
+
+
+#2. piemers
+#NAME="Vārds Uzvārds"
+#readonly NAME
+#echo $NAME
+#NAME"Uzvards Vards"
+#echo $NAME
+
+#1.piemers
+#NAME="Vārds Uzvārds"
+#echo $NAME
+
+
+$0. piemers
+history > history_20170927.txt
