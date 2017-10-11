2fdb67a4b4c4955eb074a2a07e3a14e28f95d7d5
dcr1586@inbox.lv <dcr1586@inbox.lv>
2017-10-11 15:29:51 +0300

    20171011_15_30

--- shell_operators.sh
+++ shell_operators.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
+
+# 4.piemers - operacijas ar argumentiem
+if [ $# == 2 ]
+# ja (if) skriptam nodoto argumentu skaits ($#) ir vienads (==) ar $2
+then
+# tad tiek izpilditas visas sekojosas darbibas lidz ...
+a=$1
+b=$2
+val41=`expr $a + $b`
+echo "$a + $b = "$val41
+val42=`expr $a - $b`
+echo "$a  - $b = "$val42
+val43=`expr $a \* $b`
+echo "$a * $b = "$val43
+val44=`expr $a / $b`
+echo "$a / $b = "$val44
+val45=`expr $a % $b`
+echo "$a % $b = "$val45
+# ... sai vietai
+fi
+
 # 3.piemers - operacijas ar mainigajiem
+: <<'END'
 a=56
 b=32
 val31=`expr $a + $b`
@@ -13,7 +35,7 @@ val34=`expr $a / $b`
 echo "$a / $b = "$val34
 val35=`expr $a % $b`
 echo "$a % $b = "$val35
-
+END
 
 # 2. piemers - operacijas ar konstantem
 : <<'END'
@@ -43,5 +65,5 @@ val15=`expr 2+2`
 echo "2 + 2rezultats (neparasti apostrofi, nav atstarpes) "$val15
 val16=`expr 2 + 2`
 echo "2 + 2rezultats (neparasti apostrofi, ir atstarpes) "$val16
-#END
+END
 # komentara bloka beigas
